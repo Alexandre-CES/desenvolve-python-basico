d12d05e5c0adc8a1ca72773e03cb5411d88dfcda
Alexandre <147560376+Alexandre-CES@users.noreply.github.com>
2024-04-21 18:49:51 -0300

    Add files via upload

---
+++ modulo2/aula2_questao1.py
@@ -0,0 +1,9 @@
+x = 5
+y = 2
+
+print(type(x), type(y))
+
+z = 5 / 2
+
+print(z)
+print(type(z))
---
+++ modulo2/aula2_questao2.py
@@ -0,0 +1,11 @@
+frase = 'O resultado é'
+x = 10
+y = 3.5
+
+resultado = x + y
+
+print(frase, type(frase))
+print(x, type(x))
+print(y, type(y))
+
+print(resultado, type(resultado))
